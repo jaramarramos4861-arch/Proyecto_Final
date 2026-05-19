a460cb73bb93300b1efa447cad5255caea9e2815
cristianbarba7670-cpu <cristian.barba7670@alumnos.udg.mx>
2026-05-18 22:02:05 -0600

    Creacion de ruta

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Paletas;
 use Illuminate\Support\Facades\Route;
+Route::resource('/Paletas', Paletas::class);
 
 Route::get('/', function () {
     return view('welcome');
